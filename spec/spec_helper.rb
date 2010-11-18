5d7ba3999eff2aec361e67e77fb50ed5a32f9e0c
Grant Hutchins <pair+grant@pivotallabs.com>
2010-11-18 16:51:56 -0500

    with_model has been extracted to a new gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,56 +36,11 @@ end
 install_contrib_module_if_missing("pg_trgm", "SELECT 'abcdef' % 'cdef'", "t")
 install_contrib_module_if_missing("unaccent", "SELECT unaccent('foo')", "foo")
 
-module WithModel
-  class Dsl
-    attr_reader :model_initialization
+require "with_model"
 
-    def initialize(name, example_group)
-      dsl = self
-
-      @example_group = example_group
-      @table_name = table_name = "with_model_#{name}_#{$$}"
-      @model_initialization = lambda {}
-
-      example_group.class_eval do
-        attr_accessor name
-      end
-
-      example_group.before do
-        send("#{name}=", Class.new(ActiveRecord::Base) do
-          set_table_name table_name
-          self.instance_eval(&dsl.model_initialization)
-        end)
-      end
-    end
-
-    def table(&block)
-      @example_group.with_table(@table_name, &block)
-    end
-
-    def model(&block)
-      @model_initialization = block
-    end
-  end
-
-  def with_model(name, &block)
-    Dsl.new(name, self).instance_eval(&block)
-  end
-
-  def with_table(name, &block)
-    connection = ActiveRecord::Base.connection
-    before do
-      connection.drop_table(name) rescue nil
-      connection.create_table(name, &block)
-    end
-
-    after do
-      connection.drop_table(name) rescue nil
-    end
-  end
+RSpec.configure do |config|
+  config.extend WithModel
 end
-
-RSpec::Core::ExampleGroup.extend WithModel
 
 if defined?(ActiveRecord::Relation)
   RSpec::Matchers::OperatorMatcher.register(ActiveRecord::Relation, '=~', RSpec::Matchers::MatchArray)
