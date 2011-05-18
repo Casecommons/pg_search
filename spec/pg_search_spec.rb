7ba1cb70dbac2ae1c5fbbe8e69a1d2275aba8f4c
Grant Hutchins & Ian Zabel <pair+grant+izabel@pivotallabs.com>
2011-05-18 15:29:24 -0400

    Extract the specs for Multisearchable.

--- spec/pg_search_spec.rb
+++ spec/pg_search_spec.rb
@@ -605,62 +605,9 @@ describe "an ActiveRecord model which includes PgSearch" do
   end
 
   describe ".multisearchable" do
-    with_model :model_that_is_multisearchable do
-      table do |t|
-      end
-      model do
-        include PgSearch
-        multisearchable
-      end
-    end
-
-    with_table "pg_search_documents", {}, &DOCUMENTS_SCHEMA
-
-    describe "a model that is multisearchable" do
-      subject { ModelThatIsMultisearchable }
-
-      describe "callbacks" do
-        describe "after_create" do
-          let(:record) { ModelThatIsMultisearchable.new }
-
-          describe "saving the record" do
-            subject do
-              lambda { record.save! }
-            end
-
-            it { should change(PgSearch::Document, :count).by(1) }
-          end
-
-          describe "the document" do
-            subject { document }
-            before { record.save! }
-            let(:document) { PgSearch::Document.last }
-
-            its(:searchable) { should == record }
-          end
-        end
-
-        describe "after_update" do
-          it "should touch its document" do
-            record = ModelThatIsMultisearchable.create!
-
-            record.pg_search_document.should_receive(:save)
-            lambda { record.save! }.should_not change(PgSearch::Document, :count)
-          end
-        end
-
-        describe "after_destroy" do
-          it "should remove its document" do
-            record = ModelThatIsMultisearchable.create!
-            document = record.pg_search_document
-
-            lambda { record.destroy }.should change(PgSearch::Document, :count).by(-1)
-            lambda {
-              PgSearch::Document.find(document.id)
-            }.should raise_error(ActiveRecord::RecordNotFound)
-          end
-        end
-      end
+    it "should include the Multisearchable module" do
+      ModelWithPgSearch.should_receive(:include).with(PgSearch::Multisearchable)
+      ModelWithPgSearch.multisearchable
     end
   end
 end
