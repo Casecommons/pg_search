c99f52cb129da16f6433896f0ee663a664c2757a
Grant Hutchins & Ian Zabel <pair+grant+izabel@pivotallabs.com>
2011-05-19 17:17:26 -0400

    Rake task for generating a basic pg_search_documents table

--- lib/pg_search/tasks.rb
+++ lib/pg_search/tasks.rb
@@ -3,6 +3,36 @@ require 'pg_search'
 
 namespace :pg_search do
   namespace :migration do
+    desc "Generate migration to add table for multisearch"
+    task :multisearch do
+      now = Time.now.utc
+      filename = "#{now.strftime('%Y%m%d%H%M%S')}_create_pg_search_documents.rb"
+
+      File.open(Rails.root + 'db' + 'migrate' + filename, 'wb') do |migration_file|
+        migration_file.puts <<-RUBY
+class CreatePgSearchDocuments < ActiveRecord::Migration
+  def self.up
+    say_with_time("Creating table for pg_search multisearch") do
+      create_table :pg_search_documents do |t|
+        t.text :content
+        t.belongs_to :searchable, :polymorphic => true
+        t.timestamps
+      end
+    end
+  end
+
+  def self.down
+    say_with_time("Dropping table for pg_search multisearch") do
+      drop_table :pg_search_documents
+    end
+  end
+end
+        RUBY
+      end
+    end
+
+
+
     desc "Generate migration to add support functions for :dmetaphone"
     task :dmetaphone do
       now = Time.now.utc
