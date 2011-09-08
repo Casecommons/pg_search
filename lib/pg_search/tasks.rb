6fe5094237c0b44dcf5f3030b8e28f4739763d1c
Max De Marzi <maxdemarzi@hotmail.com>
2011-09-08 11:45:12 -0500

    fixing syntax error

--- lib/pg_search/tasks.rb
+++ lib/pg_search/tasks.rb
@@ -4,7 +4,7 @@ require 'pg_search'
 namespace :pg_search do
   namespace :multisearch do
     desc "Rebuild PgSearch multisearch records for MODEL"
-    task rebuild: :environment do
+    task :rebuild => :environment do
       raise "must set MODEL=<model name>" unless ENV["MODEL"]
       model_class = ENV["MODEL"].classify.constantize
       PgSearch::Multisearch.rebuild(model_class)
