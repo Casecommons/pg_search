be1f234945146cd2bcefd08f8c0a239104c665f3
Grant Hutchins <nertzy@gmail.com>
2012-03-24 16:19:01 -0400

    Use simpler require for spec_helper

--- spec/associations_spec.rb
+++ spec/associations_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require "spec_helper"
 
 describe PgSearch do
   context "joining to another table" do
--- spec/pg_search/multisearch_spec.rb
+++ spec/pg_search/multisearch_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require "spec_helper"
 
 describe PgSearch::Multisearch do
   with_table "pg_search_documents", {}, &DOCUMENTS_SCHEMA
