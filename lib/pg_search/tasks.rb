27d7afa5f499fea2f1cfbd258356ddcf8cdccc67
Grant Hutchins <nertzy@gmail.com>
2012-05-13 18:39:02 -0400

    Use rake task arguments for multisearch rebuild
    The previous implementation required setting an
    environment variable, which was not
    self-documenting when running `rake -T`.

--- lib/pg_search/tasks.rb
+++ lib/pg_search/tasks.rb
@@ -3,10 +3,13 @@ require 'pg_search'
 
 namespace :pg_search do
   namespace :multisearch do
-    desc "Rebuild PgSearch multisearch records for MODEL"
-    task :rebuild => :environment do
-      raise "must set MODEL=<model name>" unless ENV["MODEL"]
-      model_class = ENV["MODEL"].classify.constantize
+    desc "Rebuild PgSearch multisearch records for a given model"
+    task :rebuild, [:model] => :environment do |task, args|
+      raise ArgumentError, <<-MESSAGE unless args.model
+You must pass a model as an argument.
+Example: rake pg_search:multisearch:rebuild[BlogPost]
+      MESSAGE
+      model_class = args.model.classify.constantize
       PgSearch::Multisearch.rebuild(model_class)
     end
   end
