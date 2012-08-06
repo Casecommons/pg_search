5a885821510facd15508797000ea4b0dae880644
Grant Hutchins <nertzy@gmail.com>
2012-08-06 14:04:06 -0400

    Refactor PgSearch::Assocation

--- lib/pg_search/configuration/association.rb
+++ lib/pg_search/configuration/association.rb
@@ -18,20 +18,30 @@ module PgSearch
       end
 
       def join(primary_key)
-        selects = columns.map do |column|
-          case @model.connection.send(:postgresql_version)
-          when 0..90000
-            "array_to_string(array_agg(#{column.full_name}), ' ') AS #{column.alias}"
-          else
-            "string_agg(#{column.full_name}::text, ' ') AS #{column.alias}"
-          end
-        end.join(", ")
-        relation = @model.joins(@name).select("#{primary_key} AS id, #{selects}").group(primary_key)
-        "LEFT OUTER JOIN (#{relation.to_sql}) #{subselect_alias} ON #{subselect_alias}.id = #{primary_key}"
+        "LEFT OUTER JOIN (#{relation(primary_key).to_sql}) #{subselect_alias} ON #{subselect_alias}.id = #{primary_key}"
       end
 
       def subselect_alias
         Configuration.alias(table_name, @name, "subselect")
+      end
+
+      private
+
+      def selects
+        postgresql_version = @model.connection.send(:postgresql_version)
+
+        columns.map do |column|
+          case postgresql_version
+          when 0..90000
+            "array_to_string(array_agg(#{column.full_name}::text), ' ') AS #{column.alias}"
+          else
+            "string_agg(#{column.full_name}::text, ' ') AS #{column.alias}"
+          end
+        end.join(", ")
+      end
+
+      def relation(primary_key)
+        @model.joins(@name).select("#{primary_key} AS id, #{selects}").group(primary_key)
       end
     end
   end
