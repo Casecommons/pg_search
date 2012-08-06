ef142bf59e849422f73b3da1d5dfbed34ec1cf0f
Grant Hutchins <nertzy@gmail.com>
2012-08-06 14:10:40 -0400

    Refactor PgSearch::Normalizer

--- lib/pg_search/normalizer.rb
+++ lib/pg_search/normalizer.rb
@@ -4,19 +4,19 @@ module PgSearch
       @config = config
     end
 
-    def add_normalization(original_sql)
-      normalized_sql = original_sql
+    def add_normalization(sql_expression)
       if @config.ignore.include?(:accents)
         if @config.postgresql_version < 90000
           raise PgSearch::NotSupportedForPostgresqlVersion.new(<<-MESSAGE.gsub /^\s*/, '')
-          Sorry, {:ignoring => :accents} only works in PostgreSQL 9.0 and above.
-          #{@config.inspect}
+            Sorry, {:ignoring => :accents} only works in PostgreSQL 9.0 and above.
+            #{@config.inspect}
           MESSAGE
         else
-          normalized_sql = "unaccent(#{normalized_sql})"
+          "unaccent(#{sql_expression})"
         end
+      else
+        sql_expression
       end
-      normalized_sql
     end
   end
 end
