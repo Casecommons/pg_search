2e439952f158e5a0504dbe6db9547bf53b3064d2
Grant Hutchins <nertzy@gmail.com>
2012-09-29 14:43:08 -0400

    Rebuilder honors :if on multisearchable models
    Add unit test coverage to Rebuilder

--- lib/pg_search/multisearch/rebuilder.rb
+++ lib/pg_search/multisearch/rebuilder.rb
@@ -1,17 +1,20 @@
 module PgSearch
   module Multisearch
     class Rebuilder
-      def initialize(model)
+      def initialize(model, time_source = Time.method(:now))
         unless model.respond_to?(:pg_search_multisearchable_options)
           raise ModelNotMultisearchable.new(model)
         end
 
         @model = model
+        @time_source = time_source
       end
 
       def rebuild
         if model.respond_to?(:rebuild_pg_search_documents)
           model.rebuild_pg_search_documents
+        elsif model.pg_search_multisearchable_options[:if].present?
+          model.find_each { |record| record.update_pg_search_document }
         else
           model.connection.execute(rebuild_sql)
         end
@@ -70,7 +73,7 @@ SQL
       end
 
       def current_time
-        connection.quote(connection.quoted_date(Time.now))
+        connection.quote(connection.quoted_date(@time_source.call))
       end
     end
   end
---
+++ spec/pg_search/multisearch/rebuilder_spec.rb
@@ -0,0 +1,146 @@
+require "spec_helper"
+
+describe PgSearch::Multisearch::Rebuilder do
+  with_table "pg_search_documents", {}, &DOCUMENTS_SCHEMA
+
+  describe "#rebuild" do
+    context "when the model defines .rebuild_pg_search_documents" do
+      context "and multisearchable is not conditional" do
+        with_model :Model do
+          model do
+            include PgSearch
+            multisearchable
+
+            def rebuild_pg_search_documents
+            end
+          end
+        end
+
+        it "should call .rebuild_pg_search_documents" do
+          rebuilder = PgSearch::Multisearch::Rebuilder.new(Model)
+          Model.should_receive(:rebuild_pg_search_documents)
+          rebuilder.rebuild
+        end
+      end
+
+      context "when multisearch is conditional" do
+        with_model :Model do
+          table do |t|
+            t.boolean :active
+          end
+
+          model do
+            include PgSearch
+            multisearchable :if => :active?
+
+            def rebuild_pg_search_documents
+            end
+          end
+        end
+
+        it "should call .rebuild_pg_search_documents" do
+          rebuilder = PgSearch::Multisearch::Rebuilder.new(Model)
+          Model.should_receive(:rebuild_pg_search_documents)
+          rebuilder.rebuild
+        end
+      end
+    end
+
+    context "when the model does not define .rebuild_pg_search_documents" do
+      context "when multisearchable is not conditional" do
+        with_model :Model do
+          table do |t|
+            t.string :name
+          end
+
+          model do
+            include PgSearch
+            multisearchable :against => :name
+          end
+        end
+
+        it "should not call :rebuild_pg_search_documents" do
+          rebuilder = PgSearch::Multisearch::Rebuilder.new(Model)
+
+          # stub respond_to? to return false since should_not_receive defines the method
+          original_respond_to = Model.method(:respond_to?)
+          Model.stub(:respond_to?) do |method_name, *args|
+            if method_name == :rebuild_pg_search_documents
+              false
+            else
+              original_respond_to.call(method_name, *args)
+            end
+          end
+
+          Model.should_not_receive(:rebuild_pg_search_documents)
+          rebuilder.rebuild
+        end
+
+        it "should execute the default SQL" do
+          time = DateTime.parse("2001-01-01")
+          rebuilder = PgSearch::Multisearch::Rebuilder.new(Model, lambda { time } )
+
+          expected_sql = <<-SQL
+INSERT INTO "pg_search_documents" (searchable_type, searchable_id, content, created_at, updated_at)
+  SELECT 'Model' AS searchable_type,
+         #{Model.quoted_table_name}.id AS searchable_id,
+         (
+           coalesce(#{Model.quoted_table_name}.name::text, '')
+         ) AS content,
+         '2001-01-01 00:00:00' AS created_at,
+         '2001-01-01 00:00:00' AS updated_at
+  FROM #{Model.quoted_table_name}
+          SQL
+
+          executed_sql = []
+
+          notifier = ActiveSupport::Notifications.subscribe("sql.active_record") do |name, start, finish, id, payload|
+            executed_sql << payload[:sql]
+          end
+
+          rebuilder.rebuild
+          ActiveSupport::Notifications.unsubscribe(notifier)
+
+          executed_sql.length.should == 1
+          executed_sql.first.should == expected_sql
+        end
+      end
+
+      context "when multisearchable is conditional" do
+        with_model :Model do
+          table do |t|
+            t.boolean :active
+          end
+
+          model do
+            include PgSearch
+            multisearchable :if => :active?
+          end
+        end
+
+        it "calls update_pg_search_document on each record" do
+          record1 = Model.create!(:active => true)
+          record2 = Model.create!(:active => false)
+
+          rebuilder = PgSearch::Multisearch::Rebuilder.new(Model)
+
+          # stub respond_to? to return false since should_not_receive defines the method
+          original_respond_to = Model.method(:respond_to?)
+          Model.stub(:respond_to?) do |method_name, *args|
+            if method_name == :rebuild_pg_search_documents
+              false
+            else
+              original_respond_to.call(method_name, *args)
+            end
+          end
+          Model.should_not_receive(:rebuild_pg_search_documents)
+
+          rebuilder.rebuild
+
+          record1.pg_search_document.should be_present
+          record2.pg_search_document.should_not be_present
+        end
+      end
+    end
+  end
+end
