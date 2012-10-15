c52a438f290b94da048852ed5b0eddbe4ad2a54d
Victor Olteanu <bluestar007@pcp070291pcs.it.timeinc.com>
2012-10-15 15:07:25 -0400

    modified postgres schema argument to be passed in the format [Entity,1] to rebuilding rake task

--- lib/pg_search/tasks.rb
+++ lib/pg_search/tasks.rb
@@ -4,13 +4,13 @@ require 'pg_search'
 namespace :pg_search do
   namespace :multisearch do
     desc "Rebuild PgSearch multisearch records for a given model"
-    task :rebuild, [:model] => :environment do |task, args|
+    task :rebuild, [:model,:schema] => :environment do |task, args|
       raise ArgumentError, <<-MESSAGE unless args.model
 You must pass a model as an argument.
 Example: rake pg_search:multisearch:rebuild[BlogPost]
       MESSAGE
       model_class = args.model.classify.constantize
-      ActiveRecord::Base.connection.schema_search_path = ENV["SCHEMA"] unless !ENV["SCHEMA"]
+      ActiveRecord::Base.connection.schema_search_path = args.schema unless !args.schema
       PgSearch::Multisearch.rebuild(model_class)
     end
   end
