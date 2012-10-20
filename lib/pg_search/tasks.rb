4dba37d62ca060e352e29fd94a9225df616e3e3e
Grant Hutchins <nertzy@gmail.com>
2012-10-20 15:46:07 -0400

    Prefer "if" to "unless !"

--- lib/pg_search/tasks.rb
+++ lib/pg_search/tasks.rb
@@ -10,7 +10,7 @@ You must pass a model as an argument.
 Example: rake pg_search:multisearch:rebuild[BlogPost]
       MESSAGE
       model_class = args.model.classify.constantize
-      ActiveRecord::Base.connection.schema_search_path = args.schema unless !args.schema
+      ActiveRecord::Base.connection.schema_search_path = args.schema if args.schema
       PgSearch::Multisearch.rebuild(model_class)
     end
   end
