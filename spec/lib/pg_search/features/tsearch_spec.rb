263a0b0f097ebbde733b14d6243100b5e3eadda5
Grant Hutchins <pair+grant@pivotallabs.com>
2013-04-02 14:24:02 -0400

    Add unit test coverage for TSearch#rank

---
+++ spec/lib/pg_search/features/tsearch_spec.rb
@@ -0,0 +1,27 @@
+require "spec_helper"
+
+describe PgSearch::Features::TSearch do
+  describe "#rank" do
+    with_model :Model do
+      table do |t|
+        t.string :name
+        t.text :content
+      end
+    end
+
+    it "returns an expression using the ts_rank() function" do
+      query = "query"
+      columns = [
+        PgSearch::Configuration::Column.new(:name, nil, Model),
+        PgSearch::Configuration::Column.new(:content, nil, Model),
+      ]
+      options = {}
+      config = stub(:config, :ignore => [])
+      normalizer = PgSearch::Normalizer.new(config)
+
+      feature = described_class.new(query, options, columns, Model, normalizer)
+      feature.rank.to_sql.should ==
+        %Q{(ts_rank((to_tsvector('simple', coalesce(#{Model.quoted_table_name}."name"::text, '')) || to_tsvector('simple', coalesce(#{Model.quoted_table_name}."content"::text, ''))), (to_tsquery('simple', ''' ' || 'query' || ' ''')), 0))}
+    end
+  end
+end
