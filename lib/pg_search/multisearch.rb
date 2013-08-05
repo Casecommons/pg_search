6de167f1020798b83db2f20e8dd777408e074492
Guillaume Pierronnet <guillaume.pierronnet@gmail.com>
2013-08-05 16:46:12 +0200

    no transaction isolation

--- lib/pg_search/multisearch.rb
+++ lib/pg_search/multisearch.rb
@@ -4,10 +4,8 @@ module PgSearch
 
     class << self
       def rebuild(model, clean_up=true)
-        model.transaction do
-          PgSearch::Document.where(:searchable_type => model.name).delete_all if clean_up
-          Rebuilder.new(model).rebuild
-        end
+        PgSearch::Document.where(:searchable_type => model.name).delete_all if clean_up
+        Rebuilder.new(model).rebuild
       end
     end
 
