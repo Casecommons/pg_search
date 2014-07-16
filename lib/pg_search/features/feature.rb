f065fdd5cd64cbee9e7ce9486cc414ebc5a60640
Grant Hutchins <nertzy@gmail.com>
2014-07-15 21:43:19 -0500

    Refactor :only column filtering to private method
    I prefer not to do any unnecessary calculations in #initialize, so that
    objects are easier to instantiate for unit testing.

--- lib/pg_search/features/feature.rb
+++ lib/pg_search/features/feature.rb
@@ -5,26 +5,30 @@ module PgSearch
     class Feature
       delegate :connection, :quoted_table_name, :to => :'@model'
 
-      def initialize(query, options, columns, model, normalizer)
+      def initialize(query, options, all_columns, model, normalizer)
         @query = query
         @options = options || {}
-        if @options[:only]
-          @columns = columns.select do
-            |c| [options[:only]].flatten.map(&:to_s).include? c.name 
-          end
-        else
-          @columns = columns
-        end
+        @all_columns = all_columns
         @model = model
         @normalizer = normalizer
       end
 
       private
 
-      attr_reader :query, :options, :columns, :model, :normalizer
+      attr_reader :query, :options, :all_columns, :model, :normalizer
 
       def document
         columns.map { |column| column.to_sql }.join(" || ' ' || ")
+      end
+
+      def columns
+        if options[:only]
+          all_columns.select do |column|
+            Array.wrap(options[:only]).map(&:to_s).include? column.name
+          end
+        else
+          all_columns
+        end
       end
 
       def normalize(expression)
