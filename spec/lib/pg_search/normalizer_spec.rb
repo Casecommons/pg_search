5e3853ba6894f02be3c5218a28e8027a744f071e
Grant Hutchins <nertzy@gmail.com>
2016-12-19 15:04:34 -0600

    Clean up spec

--- spec/lib/pg_search/normalizer_spec.rb
+++ spec/lib/pg_search/normalizer_spec.rb
@@ -2,58 +2,56 @@ require "spec_helper"
 
 describe PgSearch::Normalizer do
   describe "#add_normalization" do
-    context "for PostgreSQL 9.0 and above" do
-      context "when config[:ignore] includes :accents" do
-        context "when passed an Arel node" do
-          it "wraps the expression in unaccent()" do
-            config = double("config", :ignore => [:accents], :postgresql_version => 90000)
-            node = Arel::Nodes::NamedFunction.new("foo", [Arel::Nodes.build_quoted("bar")])
+    context "when config[:ignore] includes :accents" do
+      context "when passed an Arel node" do
+        it "wraps the expression in unaccent()" do
+          config = double("config", :ignore => [:accents], :postgresql_version => 90000)
+          node = Arel::Nodes::NamedFunction.new("foo", [Arel::Nodes.build_quoted("bar")])
 
-            normalizer = PgSearch::Normalizer.new(config)
-            expect(normalizer.add_normalization(node)).to eq("unaccent(foo('bar'))")
-          end
-
-          context "when a custom unaccent function is specified" do
-            it "wraps the expression in that function" do
-              allow(PgSearch).to receive(:unaccent_function).and_return("my_unaccent")
-              node = Arel::Nodes::NamedFunction.new("foo", [Arel::Nodes.build_quoted("bar")])
-
-              config = double("config", :ignore => [:accents], :postgresql_version => 90000)
-
-              normalizer = PgSearch::Normalizer.new(config)
-              expect(normalizer.add_normalization(node)).to eq("my_unaccent(foo('bar'))")
-            end
-          end
+          normalizer = PgSearch::Normalizer.new(config)
+          expect(normalizer.add_normalization(node)).to eq("unaccent(foo('bar'))")
         end
 
-        context "when passed a String" do
-          it "wraps the expression in unaccent()" do
+        context "when a custom unaccent function is specified" do
+          it "wraps the expression in that function" do
+            allow(PgSearch).to receive(:unaccent_function).and_return("my_unaccent")
+            node = Arel::Nodes::NamedFunction.new("foo", [Arel::Nodes.build_quoted("bar")])
+
             config = double("config", :ignore => [:accents], :postgresql_version => 90000)
 
             normalizer = PgSearch::Normalizer.new(config)
-            expect(normalizer.add_normalization("foo")).to eq("unaccent(foo)")
-          end
-
-          context "when a custom unaccent function is specified" do
-            it "wraps the expression in that function" do
-              allow(PgSearch).to receive(:unaccent_function).and_return("my_unaccent")
-
-              config = double("config", :ignore => [:accents], :postgresql_version => 90000)
-
-              normalizer = PgSearch::Normalizer.new(config)
-              expect(normalizer.add_normalization("foo")).to eq("my_unaccent(foo)")
-            end
+            expect(normalizer.add_normalization(node)).to eq("my_unaccent(foo('bar'))")
           end
         end
       end
 
-      context "when config[:ignore] does not include :accents" do
-        it "passes the expression through" do
-          config = double("config", :ignore => [], :postgresql_version => 90000)
+      context "when passed a String" do
+        it "wraps the expression in unaccent()" do
+          config = double("config", :ignore => [:accents], :postgresql_version => 90000)
 
           normalizer = PgSearch::Normalizer.new(config)
-          expect(normalizer.add_normalization("foo")).to eq("foo")
+          expect(normalizer.add_normalization("foo")).to eq("unaccent(foo)")
         end
+
+        context "when a custom unaccent function is specified" do
+          it "wraps the expression in that function" do
+            allow(PgSearch).to receive(:unaccent_function).and_return("my_unaccent")
+
+            config = double("config", :ignore => [:accents], :postgresql_version => 90000)
+
+            normalizer = PgSearch::Normalizer.new(config)
+            expect(normalizer.add_normalization("foo")).to eq("my_unaccent(foo)")
+          end
+        end
+      end
+    end
+
+    context "when config[:ignore] does not include :accents" do
+      it "passes the expression through" do
+        config = double("config", :ignore => [], :postgresql_version => 90000)
+
+        normalizer = PgSearch::Normalizer.new(config)
+        expect(normalizer.add_normalization("foo")).to eq("foo")
       end
     end
   end
