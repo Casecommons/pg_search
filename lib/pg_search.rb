b55d01a34a54e73b443faf49a6b3d961b4da08a3
Grant Hutchins <nertzy@gmail.com>
2019-07-12 22:57:07 -0500

    Extract PgSearch::Model module
    `include PgSearch` is now deprecated and will be removed in version 3.0. 
    Please use `include PgSearch::Model` instead. 
    
    This helps prevent namespace collisions when trying to access classes
    whose names match other classes within the PgSearch module.
    
    For example, calling `Document.new` in a model with `include PgSearch`
    will instantiate a PgSearch::Document instance rather than a ::Document,
    violating the Principal of Least Surprise.
    
    Fixes #408

--- lib/pg_search.rb
+++ lib/pg_search.rb
@@ -7,6 +7,7 @@ require "active_support/core_ext/string/strip"
 
 require "pg_search/configuration"
 require "pg_search/features"
+require "pg_search/model"
 require "pg_search/multisearch"
 require "pg_search/multisearchable"
 require "pg_search/normalizer"
@@ -14,37 +15,21 @@ require "pg_search/scope_options"
 require "pg_search/version"
 
 module PgSearch
-  extend ActiveSupport::Concern
+  def self.included(base)
+    ActiveSupport::Deprecation.warn <<-MESSAGE.strip_heredoc
+      Directly including `PgSearch` into an Active Record model is deprecated and will be removed in pg_search 3.0.
+
+      Please replace `include PgSearch` with `include PgSearch::Model`.
+    MESSAGE
+
+    base.include PgSearch::Model
+  end
 
   mattr_accessor :multisearch_options
   self.multisearch_options = {}
 
   mattr_accessor :unaccent_function
   self.unaccent_function = "unaccent"
-
-  module ClassMethods
-    def pg_search_scope(name, options)
-      options_proc = if options.respond_to?(:call)
-                       options
-                     elsif options.respond_to?(:merge)
-                       ->(query) { { query: query }.merge(options) }
-                     else
-                       raise ArgumentError, 'pg_search_scope expects a Hash or Proc'
-                     end
-
-      define_singleton_method(name) do |*args|
-        config = Configuration.new(options_proc.call(*args), self)
-        scope_options = ScopeOptions.new(config)
-        scope_options.apply(self)
-      end
-    end
-
-    def multisearchable(options = {})
-      include PgSearch::Multisearchable
-      class_attribute :pg_search_multisearchable_options
-      self.pg_search_multisearchable_options = options
-    end
-  end
 
   class << self
     def multisearch(*args)
@@ -64,32 +49,6 @@ module PgSearch
       else
         true
       end
-    end
-  end
-
-  def method_missing(symbol, *args)
-    case symbol
-    when :pg_search_rank
-      raise PgSearchRankNotSelected unless respond_to?(:pg_search_rank)
-
-      read_attribute(:pg_search_rank).to_f
-    when :pg_search_highlight
-      raise PgSearchHighlightNotSelected unless respond_to?(:pg_search_highlight)
-
-      read_attribute(:pg_search_highlight)
-    else
-      super
-    end
-  end
-
-  def respond_to_missing?(symbol, *args)
-    case symbol
-    when :pg_search_rank
-      attributes.key?(:pg_search_rank)
-    when :pg_search_highlight
-      attributes.key?(:pg_search_highlight)
-    else
-      super
     end
   end
 
